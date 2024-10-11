db0eb74054581f1f8e2b98f2b8e1124c3a9b4948
kent <bibliethumped@gmail.com>
2024-10-11 23:16:58 +0300

    Add documentatiom about circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус окружности r, возвращает площадь треугольника(float)'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус окружности r, возвращает её длину'''
     return 2 * math.pi * r
 
